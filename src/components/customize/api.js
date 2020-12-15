f58eb625b1662011ce414f4006e0aca5c38a4b68
贺贺 <zhulinger520@163.com>
2020-12-15 23:57:36 +0800

    fix: 修改服务器地址

--- src/components/customize/api.js
+++ src/components/customize/api.js
@@ -1,6 +1,6 @@
 let URL = 'http://localhost:3002/api/';
 if(window.location.host === '47.105.170.226') {
-    URL = 'http://47.105.170.226'
+    URL = 'http://47.105.170.226/api/'
 }
 
 export default {
