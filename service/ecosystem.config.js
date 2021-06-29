669506714fb745125674ed37e9dc9a23f733ab7e
贺贺 <yingchun.he@100credit.com>
2021-06-29 19:47:54 +0800

    feat： 添加ecosystem.config.js

---
+++ service/ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    apps: [
+        {
+            name: 'genetate',
+            script: './index.js',
+            instances: 1,
+            watch: true,
+            ignore_watch: ['[/\\]./', 'node_modules', 'tmp']
+        }
+    ],
+    deploy: {
+        daily: {
+            user: 'root',
+            host: ['47.105.170.226'],
+            ref: 'origin/node',
+            repo: 'git@github.com:springHyc/bms-code-auto-generate.git',
+            path: '/srv/bms-code-auto-generate',
+            'post-deploy': ' git pull  && npm run daily',
+            env: {
+                NODE_ENV: 'daily'
+            }
+        }
+    }
+};
