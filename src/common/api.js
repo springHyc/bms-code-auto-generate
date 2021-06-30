2ca6c67e52b7f1518d8f25ecc31f7b8bb0e7c739
贺贺 <yingchun.he@100credit.com>
2021-06-30 14:17:41 +0800

    feat: test

--- src/common/api.js
+++ src/common/api.js
@@ -1,6 +1,16 @@
-let URL = 'http://localhost:3002/api/';
-if(window.location.host === '47.105.170.226') {
-    URL = 'http://47.105.170.226/api/'
+// let URL = 'http://localhost:3002/api/';
+// console.log('window.location.host=', window.location.host);
+// if (window.location.host === '47.105.170.226') {
+//     URL = 'http://47.105.170.226/api/';
+// }
+
+// export default {
+//     URL
+// };
+let URL = 'http://localhost:3003/api/';
+console.log('window.location.host=', window.location.host);
+if (window.location.host === '47.105.170.226:8080') {
+    URL = 'http://47.105.170.226/api/';
 }
 
 export default {
