6bc91e70c2edb17da730b8df8e5cbb0b287d64a8
贺贺 <yingchun.he@100credit.com>
2021-06-30 14:34:24 +0800

    test

--- service/ecosystem.config.js
+++ service/ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
             host: ['47.105.170.226'],
             ref: 'origin/node',
             repo: 'git@github.com:springHyc/bms-code-auto-generate.git',
-            path: '/srv/bms-code-auto-generate/',
+            path: '/srv/hehe/bms-code-auto-generate3',
             'post-deploy': 'git pull  && cd service && npm run daily',
             env: {
                 NODE_ENV: 'daily'
