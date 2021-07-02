0d9f797e600ccf9de72bf829c596c72e8e6e18cd
贺贺 <yingchun.he@100credit.com>
2021-07-02 15:35:28 +0800

    test pm2 发布

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,5 +19,18 @@ module.exports = {
                 PM2_SERVE_HOMEPAGE: '/index.html'
             }
         }
-    ]
+    ],
+    deploy: {
+        daily: {
+            user: 'root',
+            host: ['47.105.170.226'],
+            ref: 'origin/node',
+            repo: 'git@github.com:springHyc/bms-code-auto-generate.git',
+            path: '/srv/hehe/bms-code-auto-generate3',
+            'post-deploy': 'git pull && npm install && npm run daily',
+            env: {
+                NODE_ENV: 'daily'
+            }
+        }
+    }
 };
