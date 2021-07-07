aad1f43316aa959009c30f9e12c3427cd38340ee
贺贺 <yingchun.he@100credit.com>
2021-07-07 11:55:17 +0800

    fix: 修改线上接口判断方式

--- src/common/api.js
+++ src/common/api.js
@@ -9,7 +9,7 @@
 // };
 let URL = 'http://localhost:3003/api/';
 console.log('window.location.host=', window.location.host);
-if (window.location.host === '47.105.170.226:8080') {
+if (window.location.host.indexOf('47.105.170.226') !== -1) {
     URL = 'http://47.105.170.226:3003/api/';
 }
 
